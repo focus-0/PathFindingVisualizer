914a45124265c54b43a20ec8b23de8502e34a862
Ayush Lohumi <ayushlohumi@MacBook-Air-2.local>
2025-06-05 05:38:55 +0530

    starting up

---
+++ script.js
@@ -0,0 +1,40 @@
+let startx=0;
+let starty=0;
+
+let endx=0;
+let endy=0;
+
+const gridContainer = document.querySelector('.grid'); 
+
+function createGrid(rows, cols) {
+    gridContainer.innerHTML = ''; 
+
+    for (let row = 0; row < rows; row++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+
+        for (let col = 0; col < cols; col++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.id = `cell-${row}-${col}`;
+            rowDiv.appendChild(cell);
+        }
+
+        gridContainer.appendChild(rowDiv);
+    }
+}
+
+// Call the function
+createGrid(40, 40);
+
+
+
+function randomMaze(Maze){
+
+}
+
+
+function dijkstra(Maze){
+
+}
+
